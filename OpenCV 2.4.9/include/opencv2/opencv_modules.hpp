863ee716c45b6ff152643c2577bcaaf20411e853
praveen <praveen@praveen-desktop.(none)>
2013-07-05 20:56:18 +0530

    Opencv 2.4.9 built with SIMD  for pandaboard ES: armv7 architecture, hardfloat utilizing VFPv3 and NEON

---
+++ OpenCV 2.4.9/include/opencv2/opencv_modules.hpp
@@ -0,0 +1,39 @@
+/*
+ *      ** File generated automatically, do not modify **
+ *
+ * This file defines the list of modules available in current build configuration
+ *
+ *
+*/
+
+#define HAVE_OPENCV_CALIB3D
+#define HAVE_OPENCV_CONTRIB
+#define HAVE_OPENCV_CORE
+#define HAVE_OPENCV_FEATURES2D
+#define HAVE_OPENCV_FLANN
+#define HAVE_OPENCV_GPU
+#define HAVE_OPENCV_GPUARITHM
+#define HAVE_OPENCV_GPUBGSEGM
+#define HAVE_OPENCV_GPUCODEC
+#define HAVE_OPENCV_GPUFEATURES2D
+#define HAVE_OPENCV_GPUFILTERS
+#define HAVE_OPENCV_GPUIMGPROC
+#define HAVE_OPENCV_GPUOPTFLOW
+#define HAVE_OPENCV_GPUSTEREO
+#define HAVE_OPENCV_GPUWARPING
+#define HAVE_OPENCV_HIGHGUI
+#define HAVE_OPENCV_IMGPROC
+#define HAVE_OPENCV_LEGACY
+#define HAVE_OPENCV_ML
+#define HAVE_OPENCV_NONFREE
+#define HAVE_OPENCV_OBJDETECT
+#define HAVE_OPENCV_PHOTO
+#define HAVE_OPENCV_SOFTCASCADE
+#define HAVE_OPENCV_STITCHING
+#define HAVE_OPENCV_SUPERRES
+#define HAVE_OPENCV_TS
+#define HAVE_OPENCV_VIDEO
+#define HAVE_OPENCV_VIDEOSTAB
+#define HAVE_OPENCV_WORLD
+
+
