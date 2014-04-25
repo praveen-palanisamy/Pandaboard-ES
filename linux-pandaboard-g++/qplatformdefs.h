afc59b462eb81238f702117b607b17aa2bf0b685
praveenofpersia <praveenofpersia@gmail.com>
2014-04-25 12:01:28 +1000

    Added qmake.conf and qplatformdefs.h for building Qt5 on Pandaboard

---
+++ linux-pandaboard-g++/qplatformdefs.h
@@ -0,0 +1,17 @@
+/*
+ *   qplatformdefs.h
+ *  
+ *   Copyright (C) 2010 Texas Instruments Incorporated - http://www.ti.com/
+ *     
+ *   This program is free software; you can redistribute it and/or modify 
+ *   it under the terms of the GNU Lesser General Public License as
+ *   published by the Free Software Foundation version 2.1 of the License.
+ *         
+ *   This program is distributed .as is. WITHOUT ANY WARRANTY of any kind,
+ *   whether express or implied; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *   Lesser General Public License for more details.
+ *              
+ */
+
+#include "../../linux-g++/qplatformdefs.h"
